b184201c731c940dda8742328a2550c7c867e73a
Linn L <41192228+nettlepetal@users.noreply.github.com>
2018-09-21 19:08:46 +0200

    Made the laser function recursive, now the laser can bounce off multiple reflective objects

---
+++ ComingProject/Assets/LaserActivatedItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserActivatedItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        gameObject.layer = LayerMask.NameToLayer("LaserActivated");
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void HitByLaser()
+    {
+        Debug.Log("You hit a laser activated object!");
+    }
+}
--- ComingProject/Assets/gun.cs
+++ ComingProject/Assets/gun.cs
@@ -31,16 +31,18 @@ public class gun : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             gunFiring = true;
+
+            
         }
         else if(Input.GetMouseButtonUp(0))
         {
             gunFiring = false;
         }
 
-
         if (gunFiring == true)
         {
-            FireLaser();
+            //FireLaser();
+            DrawLaser(FireLaserRecursive(rayOriginObject.transform.position, rayOriginObject.transform.forward, 0, new List<Vector3>()));
             lineRenderer.enabled = true;
         }
         else
@@ -50,17 +52,103 @@ public class gun : MonoBehaviour {
 
     }
 
-    void FireLaser()
+    List<Vector3> FireLaserRecursive(Vector3 rayOrigin, Vector3 rayDirection, int recursions, List<Vector3> linePoints)
     {
-
-        //Maybe raycast from camera to center instead, but then draw the line as gun to hitpoint
+        //Shoot beam, check if hit object or not
+        //If hit reflective object, find reflection vector
+        //Shoot new raycast along reflection vector
+        //Count recursion
+        //Recursive call
 
         RaycastHit hit;
         Vector3 endPosition;
-
-        Vector3 rayOrigin = rayOriginObject.transform.position;
         Vector3 lineOrigin = lineOriginObject.transform.position;
+
+
+        //Use lineOrigin to draw the line but rayOrigin to calculate the raycast
+        linePoints.Add(lineOrigin);
+        linePoints.RemoveRange(recursions + 1, linePoints.Count - recursions - 1);
+
+
+        //Shoot beam, check if hit object or not
+        if (Physics.Raycast(rayOrigin, rayDirection, out hit))
+        {
+
+            linePoints.Add(hit.point);
+
+            //Check if the hit object is on the Reflective layer
+            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Reflective"))
+            {
+                //Find reflection vector
+                Vector3 reflectDirection = Vector3.Reflect(rayDirection, hit.normal);
+                recursions++;
+                
+
+                //For now just a simple break if an infinite loop of reflections occurs
+                if(recursions > 20)
+                {
+                    return new List<Vector3>();
+                }
+
+                Vector3 reflectPoint = reflectDirection * laserMaxLength;
+                linePoints.Add(reflectPoint);
+
+
+                linePoints = FireLaserRecursive(hit.point, reflectDirection, recursions, linePoints);
+                return linePoints;
+
+
+            }
+            else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("LaserActivated"))
+            {
+                LaserActivatedItem item = hit.transform.gameObject.GetComponent<LaserActivatedItem>();
+                item.HitByLaser();
+
+                return linePoints;
+            }
+            else
+            {
+                return linePoints;
+
+            }
+
+        }
+        else
+        {
+            //End beam at max laser distance
+            endPosition = rayDirection * laserMaxLength;
+
+            linePoints.Add(endPosition);
+            return linePoints;
+
+
+
+        }
+
+    }
+
+    void DrawLaser(List<Vector3> linePoints)
+    {
+        lineRenderer.positionCount = linePoints.Count;
+
+        for (int i = 0; i < linePoints.Count; i++)
+        {
+            lineRenderer.SetPosition(i, linePoints[i]);
+        }
+
+    }
+
+    void FireLaser()
+    {
+
+
+        RaycastHit hit;
+        Vector3 endPosition;
+        
+        Vector3 rayOrigin = rayOriginObject.transform.position;
         Vector3 rayDirection = rayOriginObject.transform.forward;
+
+        Vector3 lineOrigin = lineOriginObject.transform.position;
 
         //Check if laser is hitting anything
         if (Physics.Raycast(rayOrigin, rayDirection, out hit))
@@ -69,14 +157,22 @@ public class gun : MonoBehaviour {
             //Check if the hit object is on the Reflective layer
             if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Reflective"))
             {
+                //Find reflection vector
+                Vector3 reflectDirection = Vector3.Reflect(rayDirection, hit.normal);
+
+
+
                 //Reflect
                 lineRenderer.positionCount = 3;
                 lineRenderer.SetPosition(0, lineOrigin);
                 lineRenderer.SetPosition(1, hit.point);
-
-                Vector3 reflectDirection = Vector3.Reflect(rayDirection, hit.normal);
-
                 lineRenderer.SetPosition(2, reflectDirection * laserMaxLength);
+
+            }
+            else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("LaserActivated"))
+            {
+                LaserActivatedItem item = hit.transform.gameObject.GetComponent<LaserActivatedItem>();
+                item.HitByLaser();
             }
             else
             {
